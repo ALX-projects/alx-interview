3cb3e07ee4474afd6c397b1d5f21ab3456cec983
imanewkm <imanewkm02@gmail.com>
2024-09-04 18:08:38 +0100

    .\0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,17 +6,22 @@ if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
+      return;
     }
+
     const charactersURL = JSON.parse(body).characters;
+
     const charactersName = charactersURL.map(
       url => new Promise((resolve, reject) => {
         request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
             reject(promiseErr);
+            return;
           }
           resolve(JSON.parse(charactersReqBody).name);
         });
-      }));
+      })
+    );
 
     Promise.all(charactersName)
       .then(names => console.log(names.join('\n')))
